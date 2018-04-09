c0acaebcc9661ce22d8f45dc3f58dea6bebc4932
Hiram Cruz <hiram0101@gmail.com>
2018-04-09 14:36:38 -0400

    moved store methods to store module

--- scripts/store.js
+++ scripts/store.js
@@ -1,60 +1,85 @@
 'use strict';
 
-const store = (function() {
-  let items = [
-    { id: cuid(), name: 'apples', checked: false },
-    { id: cuid(), name: 'oranges', checked: false },
-    { id: cuid(), name: 'milk', checked: true },
-    { id: cuid(), name: 'bread', checked: false }
+const store = (function () {
+  let items = [{
+      id: cuid(),
+      name: 'apples',
+      checked: false
+    },
+    {
+      id: cuid(),
+      name: 'oranges',
+      checked: false
+    },
+    {
+      id: cuid(),
+      name: 'milk',
+      checked: true
+    },
+    {
+      id: cuid(),
+      name: 'bread',
+      checked: false
+    }
   ];
+
   let hideCheckedItems = false;
   let searchTerm = '';
-  function findById(id){
-    return this.items.find(function(item){
+
+  function toggleCheckedFilter() {
+    this.hideCheckedItems = !this.hideCheckedItems;
+  }
+
+  function setSearchTerm(term) {
+    this.searchTerm = term;
+  }
+
+  function findById(id) {
+    return this.items.find(function (item) {
       return item.id === id;
     });
   }
-  function addItem(name){
+
+  function addItem(name) {
     try {
       Item.validateName(name);
       this.items.push(Item.create(name));
-    }
-    catch(e){
+    } catch (e) {
       console.log(e.message);
     }
   }
-  function findAndToggleChecked(id){
+
+  function findAndToggleChecked(id) {
     let item = this.findById(id);
     item.checked = !item.checked;
   }
-  function findAndUpdateName(id,newName){
-    try{
+
+  function findAndUpdateName(id, newName) {
+    try {
       Item.validateName(newName);
       let item = this.findById(id);
       item.name = newName;
-    }
-    catch(e){
+    } catch (e) {
       return `Cannot update name: ${e.message}`;
     }
   }
-  function findAndDelete(id){
-      
-   let newArray = this.items.filter(function(item){
+
+  function findAndDelete(id) {
+    let newArray = this.items.filter(function (item) {
       return item.id !== id;
     });
     this.items = newArray;
   }
 
- 
+
   return {
+    setSearchTerm,
+    toggleCheckedFilter,
     findById,
     addItem,
     findAndToggleChecked,
     findAndUpdateName,
     findAndDelete,
     items: items,
-    hideCheckedItems: hideCheckedItems,
-    searchTerm: searchTerm,
   };
 }());
-
